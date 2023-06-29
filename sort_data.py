4a2061847b82e075354cc028392195cfca547fc6
DESKTOP-7VJ356K\cspro <cspro.r.j@gmail.com>
2023-06-29 21:01:04 +0530

    New Git Repo

--- sort_data.py
+++ sort_data.py
@@ -1,4 +1,6 @@
 import imaging_interview as img_int
+import os
+import shutil
 
 
 # img_int.draw_color_mask()
@@ -21,3 +23,27 @@ def read_img_pairs():
 
 def get_score():
     return
+
+
+######################################################################################
+#
+# sort_camera_views: This function sorts images into separate folders as per the camera id
+#
+######################################################################################
+def sort_camera_views(folder_name:str):
+    for (root, dirs, files) in os.walk(folder_name, topdown=True):
+        for filename in files:
+            working_path = os.path.abspath(folder_name)
+            file_src = working_path + '\\' + filename
+            file_dest = working_path + '\\' + filename[0:3]  # assuming file naming convention is in 'c##' format
+            if not os.path.exists(file_dest):
+                os.makedirs(file_dest)
+            shutil.move(file_src, file_dest)
+
+
+data_folder = "dataset"
+sort_camera_views(data_folder)  # Only run once to organize data
+
+
+
+# C20 uses two different naming convention even though came camera
