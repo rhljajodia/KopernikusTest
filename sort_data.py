b3da8d6226a07c145707431dbf24354886baf27a
DESKTOP-7VJ356K\cspro <cspro.r.j@gmail.com>
2023-06-29 19:19:07 +0530

    New Git Repo

---
+++ sort_data.py
@@ -0,0 +1,23 @@
+import imaging_interview as img_int
+
+
+# img_int.draw_color_mask()
+
+def display_images():
+    return
+
+
+def get_file_list():
+    return
+
+
+def parse_file_list():
+    return
+
+
+def read_img_pairs():
+    return
+
+
+def get_score():
+    return
